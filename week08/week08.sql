c37e8450ab9f2e6704589923e8519213ca269ebc
GORILLA <c.a.fletcher@wustl.edu>
2026-03-22 16:47:45 +0000

    code changes for hw

---
+++ week08/week08.sql
@@ -0,0 +1,6 @@
+#                Product / Ingredients / Facility / Consumer / Employee
+#Manufacturing      X            X          X          X         X           
+#Delivery           X            X          X          X         X
+#Testing            X                                            X
+#Sanitation         X            X          X                    X
+#Shift Rotation                             X                    X
